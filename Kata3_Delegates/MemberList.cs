06473924f4d4e9752b352cf0b44dcedac482cb2f
Martin Lenart <martin_lenart@icloud.com>
2022-03-08 13:01:48 +0100

    Delegate finalized

---
+++ Kata3_Delegates/MemberList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kata3_Delegates
+{
+    class MemberList : IMemberList
+    {
+        List<IMember> _members = new List<IMember>();
+
+        public IMember this[int idx]
+        {
+            get { return _members[idx]; }
+        }
+        public int Count() => _members.Count;
+        public int Count(int year)
+        {
+            int c = 0;
+            foreach (var item in _members)
+            {
+                if (item.Since.Year == year)
+                    c++;
+            }
+            return c;
+        }
+
+        public void Sort() => _members.Sort();
+
+        public override string ToString()
+        {
+            string sRet = "";
+            for (int i = 0; i < _members.Count; i++)
+            {
+                sRet += $"{_members[i]}\n";
+                if ((i + 1) % 10 == 0)
+                {
+                    sRet += "\n";
+                }
+            }
+            return sRet;
+        }
+
+        #region Class Factory for creating an instance filled with Random data
+        internal static class Factory
+        {
+            internal static IMemberList CreateWithRandomData(int NrOfItems, FredriksAction fredriksAction)
+            {
+                var memberlist = new MemberList();
+                for (int i = 0; i < NrOfItems; i++)
+                {
+                    var newMember = Member.Factory.CreateWithRandomData();
+                    memberlist._members.Add(newMember);
+
+                    fredriksAction(newMember);
+                }
+                return memberlist;
+            }
+        }
+        #endregion
+
+        public MemberList() { }
+    }
+    delegate void FredriksAction(IMember member);
+}
